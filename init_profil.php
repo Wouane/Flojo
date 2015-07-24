8a8bc1b00e63f73124fc956497d3a4693da49b6d
intdevweb <jp_informatique@hotmail.fr>
2015-07-24 14:41:51 +0200

    indentation

--- init_profil.php
+++ init_profil.php
@@ -1,6 +1,6 @@
 <?php 
-	session_start();
-	include("functions.php");
+session_start();
+include("functions.php");
 	//pr($_SESSION);
 
 	//vérification que l'utilisateur est bien connecté
@@ -23,38 +23,38 @@
 
 	<!--<h1>Profil de <?php echo $_SESSION['user']['username']; ?></h1>-->
 	
-<h1>FLOJO init_Profil</h1> 
+	<h1>FLOJO init_Profil</h1> 
 	<!--!!!!!!!!!!!!!attention  "action" doit pointer sur profil.php!!!!!!!!!!!!!! -->
 	<form method="POST" action="init_profil.php">
 		<label for="description">Inserer une photo</label>
 	</br>
-		<input type="file" name="user_picture"/>
-	</br>
-	</br>
-	<label for="description">Entrez votre descriptif</label>
-	</br> 
-	<textarea name="user_description" id="description" rows="4" cols="50" > 
+	<input type="file" name="user_picture"/>
+</br>
+</br>
+<label for="description">Entrez votre descriptif</label>
+</br> 
+<textarea name="user_description" id="description" rows="4" cols="50" > 
 
 </textarea>
 </br>
 
-		<!--<input type="text" name="description" placeholder="Description" />-->
-		<input type="submit" value="OK" />
-	</form>
+<!--<input type="text" name="description" placeholder="Description" />-->
+<input type="submit" value="OK" />
+</form>
 </br>
 <a href="logout.php" title="Me déconnecter de mon compte">Déconnexion</a>
 
-	<!--<a href="forgot_password.php">Mot de passe oublié ?</a>-->
+<!--<a href="forgot_password.php">Mot de passe oublié ?</a>-->
 
-	<?php 
+<?php 
 		//si on a stocké un message d'erreur (dans login_handler.php)
-		if(!empty($_SESSION['login_error'])){
+if(!empty($_SESSION['login_error'])){
 			//affiche le message d'erreur
-			echo $_SESSION['login_error']; 
+	echo $_SESSION['login_error']; 
 			//on a affiché le message, alors on peut le virer
-			unset($_SESSION['login_error']);
-		}
-	?>
+	unset($_SESSION['login_error']);
+}
+?>
 
 
 
