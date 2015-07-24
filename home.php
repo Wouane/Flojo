a9b7a07e54560a702533f46f6fbac0ed5baac0d8
intdevweb <jp_informatique@hotmail.fr>
2015-07-24 12:36:12 +0200

    del login

--- home.php
+++ home.php
@@ -9,26 +9,7 @@
 				<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700' rel='stylesheet' type='text/css'>
 		</head>
 		<body>
-			<h1>FLOJO Connexion</h1>
 			
-			<form method="POST" action="login_handler.php">
-				<input type="text" name="email" placeholder="Email ou pseudo" />
-				<input type="password" name="password" placeholder="Mot de passe" />
-				<input type="submit" value="OK" />
-			</form>
-
-			<a href="forgot_password.php">Mot de passe oublié ?</a>
-
-			<?php 
-		//si on a stocké un message d'erreur (dans login_handler.php)
-			if(!empty($_SESSION['login_error'])){
-			//affiche le message d'erreur
-				echo $_SESSION['login_error']; 
-			//on a affiché le message, alors on peut le virer
-				unset($_SESSION['login_error']);
-			}
-			?>
-
 			
 		</body>
 </html>
