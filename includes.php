8b442fd7bde9bd85327e48ca24f79fc839da81e5
Florian Wouane <pro.florian.michel@gmail.com>
2015-07-24 15:44:10 +0200

    create includes files for all include

---
+++ includes.php
@@ -0,0 +1,6 @@
+<?php
+
+	include("db.php"); // inclusion BDD + config
+	include("functions.php"); // inclusion de function (pr + date)
+	include("vendor/autoload.php"); // inclusion de vendor et la librairie Random Lib
+	
