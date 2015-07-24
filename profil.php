d5a51def6215dda8c62351b0a1f7d94ae61d3b2d
intdevweb <jp_informatique@hotmail.fr>
2015-07-24 17:20:26 +0200

    add profil.php

--- profil.php
+++ profil.php
@@ -1,1 +1,44 @@
-//
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+		<head>
+				<meta charset="UTF-8">
+				<title>Mot de passe oublié</title>
+				<!-- feuilles CSS -->
+				<link href="css/styles.css" type="text/css" rel="stylesheet" media="screen">
+				<!-- Font Google -->
+				<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700' rel='stylesheet' type='text/css'>
+		</head>
+		<body>
+			<div class="main-profil">
+				<h1 class="title-profil">Profil utilisateur</h1>
+				<div class="image-profil">
+				<img src="img/default.jpg" alt="photo-profil"/>
+				</div>
+			</br>
+			<label for="description">Entrez votre message</label>
+		</br>
+			<textarea name="description" id="description" rows="4" cols="50" > 
+			</textarea>
+			<div class="logout-profil">
+				<a href="logout.php" title="Me déconnecter">Déconnexion</a>
+			</div>
+			<div class="affiche-message">
+				<div class="profil-message">
+				</div>
+			</br>
+				<div class="profil-message"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, vel, aliquam, iusto, error mollitia voluptas ratione</p>
+				</div>
+			</br>
+				<div class="profil-message"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, vel, aliquam, iusto, error mollitia voluptas ratione</p> 
+				</div>
+			</br>
+				<div class="profil-message"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, vel, aliquam, iusto, error mollitia voluptas ratione</p>
+				</div>
+			</div>
+			</div>	
+			
+		</body>
