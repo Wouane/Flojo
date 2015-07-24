62006c0c3c7cf594fbc2a7df4e668c1113ed8be5
Florian Wouane <pro.florian.michel@gmail.com>
2015-07-24 12:08:20 +0200

    init home

--- home.php
+++ home.php
@@ -1,1 +1,14 @@
-//
+<!doctype html>
+<html lang="FR">
+		<head>
+				<meta charset="UTF-8">
+				<title>Page Home Flojo</title>
+				<!-- feuilles CSS -->
+				<link href="css/styles.css" type="text/css" rel="stylesheet" media="screen">
+				<!-- Font Google -->
+				<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700' rel='stylesheet' type='text/css'>
+		</head>
+		<body>
+			
+		</body>
+</html>
