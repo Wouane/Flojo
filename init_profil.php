e25a5066adac10e90769377950e1a1e8484846a9
Florian Wouane <pro.florian.michel@gmail.com>
2015-07-27 17:38:36 +0200

    add comment

--- init_profil.php
+++ init_profil.php
@@ -39,7 +39,7 @@
 			<title>Page Home Flojo</title>
 			<!-- feuilles CSS -->
 			<link href="css/styles.css" type="text/css" rel="stylesheet" media="screen">
-			<!-- Font Google -->
+			<!-- feuilles CSS -->
 			<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700' rel='stylesheet' type='text/css'>
 	</head>
 	<body>	
