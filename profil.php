cb06dbdffd6e25def438b0718ff7b8f522edc268
Florian Wouane <pro.florian.michel@gmail.com>
2015-07-27 17:38:47 +0200

    Modify profil.php

--- profil.php
+++ profil.php
@@ -2,7 +2,6 @@
 session_start();
 
 include ("includes.php");
-
 ////////////////////////////////////////////////////////////////////////////////
 ////// delete des messages si message[date_created] dépassée de 10 minutes  ////
 ////// la date expiry n'a plus vraiment d'interet....       			        ////
@@ -14,63 +13,67 @@ include ("includes.php");
 //
 // en cas de date unix integer
 // DELETE FROM message WHERE date_created < (UNIX_TIMESTAMP() - 600);
-$sql = "SELECT *
-			FROM message 
-			ORDER BY date_created DESC
-			LIMIT 5";
 
-	$sth = $dbh ->prepare($sql);
-	$sth-> execute();
-	$messages = $sth->fetchAll();
-
-/*$sql = "SELECT username, user_description
-			FROM user 
-			WHERE username
-			LIMIT 5";
-
-	$sth = $dbh ->prepare($sql);
-	$sth-> execute();
-	$messages = $sth->fetchAll();"*/	
-if(!empty($_POST)){
-// $description = "";	
-	if ($_POST['form_name'] == "2"){
-
-		$description = trim(strip_tags($_POST['description']));
-		$title = trim(strip_tags($_POST['title']));
-		$url = trim(strip_tags($_POST['url']));
-		$mess_picture = trim(strip_tags($_POST['mess_picture']));
-
-		$sql = "INSERT INTO message (id_mess, title, description, url, mess_picture, date_created, date_expiry)
-				VALUES (NULL, :title, :description, :url, :mess_picture, NOW(), NOW())";
-
+		$sql = "SELECT *
+				FROM message 
+				ORDER BY date_created DESC
+				LIMIT 5";
+		
 		$sth = $dbh->prepare($sql);
-		$sth ->bindValue(":title",$title);
-		$sth ->bindValue(":description",$description); 
-		$sth ->bindValue(":url",$url); 
-		$sth ->bindValue(":mess_picture",$mess_picture); 
-		$sth->execute();   
+		
+		$sth->execute();
+
+		$messages = $sth->fetchAll();
+
+/*		$sql = "SELECT username, user_description
+				FROM user 
+				WHERE username
+				LIMIT 5";
+
+		$sth = $dbh ->prepare($sql);
+		$sth-> execute();
+		$messages = $sth->fetchAll();"*/
+
+if(!empty($_POST)){
+
+		if ($_POST['form_name'] == "2"){
+
+			$description = trim(strip_tags($_POST['description']));
+			$title = trim(strip_tags($_POST['title']));
+			$url = trim(strip_tags($_POST['url']));
+			$mess_picture = trim(strip_tags($_POST['mess_picture']));
+	
+			$sql = "INSERT INTO message (id_mess, title, description, url, mess_picture, date_created, date_expiry)
+					VALUES (NULL, :title, :description, :url, :mess_picture, NOW(), NOW())";
+	
+			$sth = $dbh->prepare($sql);
+			$sth ->bindValue(":title",$title);
+			$sth ->bindValue(":description",$description); 
+			$sth ->bindValue(":url",$url); 
+			$sth ->bindValue(":mess_picture",$mess_picture); 
+			$sth->execute();   
 		}
 
-	$sql = "SELECT description
-			FROM message 
-			ORDER BY date_created DESC
-			LIMIT 5";
+		$sql = "SELECT description
+				FROM message 
+				ORDER BY date_created DESC
+				LIMIT 5";
+	
+		$sth = $dbh ->prepare($sql);
+		$sth-> execute();
+		$messages = $sth->fetchAll();
 
-	$sth = $dbh ->prepare($sql);
-	$sth-> execute();
-	$messages = $sth->fetchAll();
 
+		if($_POST['form_name'] == "1"){
+		$sql = "SELECT date_created, description
+				FROM message 
+				ORDER BY date_created DESC
+				LIMIT 5";
 
-if ($_POST['form_name'] == "1"){
-	$sql = "SELECT date_created, description
-			FROM message 
-			ORDER BY date_created DESC
-			LIMIT 5";
-
-	$sth = $dbh ->prepare($sql);
-	$sth-> execute();
-	$messages = $sth->fetchAll();
-}
+		$sth = $dbh ->prepare($sql);
+		$sth-> execute();
+		$messages = $sth->fetchAll();
+		}
 
 	
 //////////////////////////////////////////////////////////////////////////
@@ -81,61 +84,58 @@ if ($_POST['form_name'] == "1"){
 
 }
 
-//pr ($messages);
+//pr($messages);
 ?>
 
 <!DOCTYPE html>
 <html lang="Fr">
-	<head>
-		<meta charset="UTF-8">
-		<title>Page profil</title>
-		<link href="css/styles.css" type="text/css" rel="stylesheet" media="screen">
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700' rel='stylesheet' type='text/css'>
-	</head>
-	<body>
+		<head>
+			<meta charset="UTF-8">
+			<title>Page profil</title>
+			<!-- feuilles CSS -->
+			<link href="css/styles.css" type="text/css" rel="stylesheet" media="screen">
+			<!-- feuilles CSS -->
+			<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700' rel='stylesheet' type='text/css'>
+		</head>
+		<body>
 			<h2 class="title-profil">Bienvenue sur votre page profil<?php echo $_SESSION['user']['username']; ?></h2>
 			<div class="main-profil">
-				<div class="image-profil">
-					<img src="img/default.jpg" alt="photo-profil"/>
-					<p>username </p>
-					<p>user description table user </p>
-				</div>
+					<div class="image-profil">
+						<img src="img/default.jpg" alt="photo-profil"/>
+						<p>username </p>
+						<p>user description table user </p>
+					</div>
 
-				<div class="affiche10">
-				<form method="POST" action="profil.php" id="affiche10" novalidate="novalidate">
-				<label for="affiche10"></label>
+			<div class="affiche10">
+			<form method="POST" action="profil.php" id="affiche10" novalidate="novalidate">
 				<input type="hidden" value="1" name="form_name"/>
-				<input type="submit" class="affiche10" value="affichage 10 mn"/>
-				</form>
+				<input type="submit" class="affiche10" value="affichage 10mn"/>
+			</form>
 			</div>
 
-				<form method="POST" action="profil.php" id="add-profil-message" novalidate="novalidate" enctype="multipart/form-data">
-					
-					<div>
-					<label for="title">Saisir le Titre</label>
-					<input type="text" name="title" id="title" />
-				</div>
-			</br>
-					<label for="description">Entrez votre message</label>
-					<input type="hidden" value="2" name="form_name"/>
-					<textarea name="description" id="description" rows="4" cols="50" ></textarea>
-			</br>		
-				<div>
-					<label for="mess_picture">Inserer une photo?</label>
-						<input type="file" name="mess_picture"/>
-				</div>
-			</br>	
-				<div>
-					<label for="url">Ajouter une URL?</label>
-					<input type="url" name="url" id="url" />
-				</div>
-			</br>		
-					<input type="submit" class="add-profil-message" value="créer message"/>
-				</form>
+			<form method="POST" action="profil.php" id="add-profil-message" novalidate="novalidate" enctype="multipart/form-data">
+				<input type="hidden" value="2" name="form_name"/>
+					<!-- TITRE DU MESSAGE  -->
+				<!-- <label for="title">Saisir le Titre</label></br> -->
+				<input type="text" name="title" id="title" placeholder="Tapez votre titre"/>
 				</br>
-				<div class="logout-profil">
-					<a href="logout.php" title="Me déconnecter">Déconnexion</a>
-				</div>				
+					<!-- INTITULE DU MESSAGE  -->
+				<!-- <label for="description">Entrez votre message</label></br> -->
+				<textarea name="description" id="description" rows="4" cols="50" placeholder="Tapez votre description" ></textarea>
+				</br>		
+					<!-- UPLOADE PHOTO  -->
+				<label for="mess_picture">Inserer une photo?</label>
+				<input type="file" name="mess_picture"/>
+				</br>	
+					<!-- UPLOAD URL  -->
+				<!-- <label for="url">Ajouter une URL?</label> -->
+				<input type="url" name="url" id="url" placeholder="Votre URL" />
+				</br>	
+					<!-- CREER LE MESSAGE  -->	
+				<input type="submit" class="add-profil-message" value="créer message"/>
+			</form>
+				</br>
+				<a class="logout" href="logout.php" title="Me déconnecter">Déconnexion</a>
 			</div>
 		</br>
 	<div class="affiche-message"> 
